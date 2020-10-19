23ad0c8c8a9cfc6d8a24019cab3d28efe10c2aa1
thijsfranck <thijsfranck@gmail.com>
2020-10-19 17:41:16 +0200

    1 file modified

--- game/solution-space.ts
+++ game/solution-space.ts
@@ -1,6 +1,4 @@
 import { ILLEGAL_STARTING_CHARACTER } from "./symbols";
-import VPTree from "mnemonist/vp-tree";
-import { intersection, difference } from "mnemonist/set";
 import { permutations } from "obliterator";
 
 export function calculateSolutionSpace(
@@ -15,54 +13,4 @@ export function calculateSolutionSpace(
     }
   }
   return solutionSpace;
-}
-
-function calculateRemainingSolutions(
-  solutionSpace: Set<string>,
-  searchTree: VPTree<string>,
-  guess: string,
-  validPositions: number
-): Set<string> {
-  if (validPositions === 0) return solutionSpace;
-  const neighbors = searchTree.neighbors(guess.length - validPositions, guess);
-  return new Set(neighbors.map(neighbor => neighbor.item));
-}
-
-export function updateSolutionSpace(
-  currentSymbolSpace: Set<string>,
-  currentSolutionSpace: Set<string>,
-  bullsSearchTree: VPTree<string>,
-  cowsSearchTree: VPTree<string>,
-  guess: string,
-  bulls: number = 0,
-  cows: number = 0
-): [Set<string>, Set<string>] {
-  let validSymbolSpace = currentSymbolSpace,
-    validSolutionSpace = currentSolutionSpace;
-
-  if (bulls > 0 || cows > 1) {
-    const byBulls = calculateRemainingSolutions(
-        validSolutionSpace,
-        bullsSearchTree,
-        guess,
-        bulls
-      ),
-      byCows = calculateRemainingSolutions(
-        validSolutionSpace,
-        cowsSearchTree,
-        guess,
-        bulls + cows
-      );
-    validSolutionSpace = intersection(validSolutionSpace, byBulls, byCows);
-  } else if (cows === 0) {
-    validSymbolSpace = difference(validSymbolSpace, new Set(guess));
-
-    const updatedSolutionSpace = calculateSolutionSpace(
-      validSymbolSpace,
-      guess.length
-    );
-
-    validSolutionSpace = intersection(validSolutionSpace, updatedSolutionSpace);
-  }
-  return [validSymbolSpace, validSolutionSpace];
 }
